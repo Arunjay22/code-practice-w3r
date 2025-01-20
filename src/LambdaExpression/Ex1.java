2e413af9897b75bb11510c4182a0b5e27c9e7c1f
Arunjay22 <arunarunaj7@gmail.com>
2025-01-20 19:54:29 +0530

    lambda expression code 1

---
+++ src/LambdaExpression/Ex1.java
@@ -0,0 +1,17 @@
+package LambdaExpression;
+
+
+interface Summation{
+    int add(int a, int b);
+}
+public class Ex1  {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        Summation summation = (a1, b1) -> a1 + b1;
+
+        int add = summation.add(a, b);
+        System.out.println(add);
+
+    }
+}
